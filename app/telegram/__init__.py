cf09c79b62d95d350a6c9f7cdbfbc14eeff85ef2
Mykhailo Ziabrov <zyabrov@gmail.com>
2024-05-13 16:48:44 +0300

    fixed error with init app.telegram

--- app/telegram/__init__.py
+++ app/telegram/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('telegram', __name__, url_prefix='/telegram', template_folder='templates')
 
-from app.Telegram import routes
+from app.telegram import routes
